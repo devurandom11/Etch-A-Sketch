e03a4533118fdfbf6f4cf7309b48c76ab039c7c5
Mike Crawford <dev.random.87@gmail.com>
2022-07-20 02:07:03 -0700

    Create 16x16 grid in javascript

--- app/main.js
+++ app/main.js
@@ -1,2 +1,19 @@
 // Etch a Sketch
 
+// Get elements from DOM
+const gameBoard = document.querySelector('.gameboard');
+const options = document.querySelector('.options');
+
+
+// Create 16x16 grid of squares 
+
+for (let i = 0; i < 4; i++) {
+    const rows = document.createElement('div');
+    rows.classList = 'board-row';
+    gameBoard.appendChild(rows)
+    for (let j = 0; j < 4; j++) {
+        const squares = document.createElement('div');
+        squares.classList = 'square';
+        rows.appendChild(squares)
+    }
+}
